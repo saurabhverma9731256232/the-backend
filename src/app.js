73358be2895ec3932521290203f2656fd181512b
Saurabh Verma <saurabhverma9731256232@gmail.com>
2025-04-13 19:36:12 +0530

    add routes for health check, tweets, videos, likes, subscriptions, playlists, and dashboard

--- src/app.js
+++ src/app.js
@@ -17,7 +17,22 @@ app.use(cookieParser())
 
 //routes import
 import userRouter from './routes/user.router.js'
-
+import heathCheckRouter from './routes/heathcheck.router.js'
+import tweetRouter from './routes/tweet.router.js'
+import likeRouter from './routes/like.router.js'
+import subscriptionRouter from './routes/subscription.router.js'    
+import playlistRouter from './routes/playlist.router.js'
+import dashboardRouter from './routes/dashboard.router.js'
+import videoRouter from './routes/video.router.js'
 // routes declaratio
 app.use('/api/v1/users',userRouter)
+app.use("/api/v1/heathcheck",heathCheckRouter)
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/likes",likeRouter)
+app.use("/api/v1/subscriptions",subscriptionRouter)
+app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/dashboard",dashboardRouter)
+
+
 export default app ;
